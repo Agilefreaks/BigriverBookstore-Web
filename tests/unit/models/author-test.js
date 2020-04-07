4b4c8af1d5e3678620b6e09b70b79d8eb3a8ae18
Romeo Onisim <romeo.onisim@gmail.com>
2020-04-07 19:11:55 +0300

    add more tests to check book images

--- tests/unit/models/author-test.js
+++ tests/unit/models/author-test.js
@@ -4,7 +4,6 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Model | author', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
   test('it exists', function(assert) {
     let store = this.owner.lookup('service:store');
     let model = store.createRecord('author', {});
--- tests/unit/models/book-test.js
+++ tests/unit/models/book-test.js
@@ -5,8 +5,27 @@ module('Unit | Model | book', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let store = this.owner.lookup('service:store');
-    let model = store.createRecord('book', {});
+    const store = this.owner.lookup('service:store');
+    const model = store.createRecord('book', {});
     assert.ok(model);
+  });
+
+  test('when one or more images per book return first one', function (assert) {
+    const store = this.owner.lookup('service:store');
+
+    const photo1 = store.createRecord('photo', { uri: 'https://url-img-1.com', title: 'Img #1' });
+    const photo2 = store.createRecord('photo', { uri: 'https://url-img-2.com', title: 'Img #2' });
+    const model = store.createRecord('book', { title: 'Book #1', photos: [photo1, photo2] });
+
+    assert.equal(model.image.uri, 'https://url-img-1.com');
+  });
+
+  test('when no image provided return the placeholder', function (assert) {
+    const store = this.owner.lookup('service:store');
+
+    const placeholder = { uri: 'http://placehold.jp/d1d1d1/707070/400x300.png?text=No%20Image', title: 'No Image' };
+    const model = store.createRecord('book', { title: 'Book #1' });
+
+    assert.equal(model.image.uri, 'http://placehold.jp/d1d1d1/707070/400x300.png?text=No%20Image');
   });
 });
