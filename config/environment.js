51b0173ff011e94bbd5277f429e03d569c241a82
Romeo Onisim <romeo.onisim@gmail.com>
2020-04-07 17:19:03 +0300

    feat: fetch and display books list unformatted

--- config/environment.js
+++ config/environment.js
@@ -23,6 +23,8 @@ module.exports = function(environment) {
     }
   };
 
+  ENV.API_BASE_URL = 'https://jsonapiplayground.reyesoft.com';
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
