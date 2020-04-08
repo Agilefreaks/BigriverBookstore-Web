cd6a0c6526dc11f64905ac3de9faa2c1028ce4b1
Romeo Onisim <romeo.onisim@gmail.com>
2020-04-08 12:12:08 +0300

    move disable option tests from acceptance to integration

--- tests/integration/components/search-books-test.js
+++ tests/integration/components/search-books-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import page from 'bigriver-bookstore/tests/pages/books';
 
 module('Integration | Component | search-books', function(hooks) {
   setupRenderingTest(hooks);
@@ -10,5 +11,20 @@ module('Integration | Component | search-books', function(hooks) {
     await render(hbs`<SearchBooks />`);
 
     assert.notEqual(this.element.textContent.trim(), '');
+  });
+  test('when providing an empty string the submit is disabled', async function (assert) {
+    await render(hbs`<SearchBooks />`);
+    await page
+      .fillInSearchInput('  ');
+
+    assert.equal(page.searchDisabledPresent, true);
+  });
+
+  test('when providing a good search query the submit is not disabled', async function (assert) {
+    await render(hbs`<SearchBooks />`);
+    await page
+      .fillInSearchInput('prof');
+
+    assert.equal(page.searchDisabledPresent, false);
   });
 });
